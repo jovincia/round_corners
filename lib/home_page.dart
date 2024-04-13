a840247e7341cdbccf75188b154dde173e85a7c5
Jovincia <assogbadanielle15@gmail.com>
2024-04-14 00:58:33 +0100

    add files and news updates

---
+++ lib/home_page.dart
@@ -0,0 +1,58 @@
+import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
+import "package:round_corners/colors.dart";
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      
+      body: Center(
+        child: Column(
+          children: [ 
+               Text(
+            
+            "Made by Dani.",
+            style: TextStyle(
+              height: 5,
+              fontStyle: FontStyle.italic,
+              fontSize: 20,
+              color: MainColors.brown
+            ),
+           ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+        
+              child: Container(
+                height: 300,
+                width: 300,
+                color: MainColors.brownNude,
+              
+              
+              ),
+            )
+          ],
+        ),
+      ),
+          
+           /*  child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Container(
+                height: 300,
+                width: 300,
+                color: MainColors.brownCoffeeMilk,
+              ),
+                children: [
+              Text(
+                "Made by Dani",
+                style: TextStyle(color: MainColors.brown),
+              ),
+            ],
+            ), */
+            
+   
+    );
+  }
+}
